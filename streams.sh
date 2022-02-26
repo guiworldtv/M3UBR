33f38c12341189e08055cecf98db649a91cca4f8
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2022-02-26 12:09:33 -0300

    Update streams.sh

--- streams.sh
+++ streams.sh
@@ -12,4 +12,8 @@ sed -i "/live_cnn/ c https://video-auth7.iol.pt/live_cnn/live_cnn/playlist.m3u8?
 
 sed -i "/live_tvi_internacional/ c https://video-auth6.iol.pt/live_tvi_internacional/live_tvi_internacional/playlist.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)/" m3upt.m3u
 
+s# TV Anhanguera Goiania - update the stream URL of TV Anhanguera Goiania
+
+ed -i "hls-globo-go01/ c https://live-as-10-16.video.globo.com/d/s/hls-globo-go01/playlist.m3u8?h=$(wget https://s3.glbimg.com -o /dev/null -O -)/" m3upt.m3u 
+
 exit 0
