537cb4935cd63daea62154b49b754d70d177a2d0
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2022-08-07 04:26:23 -0300

    Update streams.sh

--- streams.sh
+++ streams.sh
@@ -4,6 +4,8 @@
 
 sed -i "/live_tvi\/live_tvi/ c https://video-auth6.iol.pt/live_tvi/live_tvi/playlist.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)/" m3upt.m3u
 
+https://video-auth4.iol.pt/live_tvi_direct/live_tvi_direct/edge_servers/tvireality-720_passthrough/chunks.m3u8?
+
 # CNN Portugal - update the stream URL of CNN Portugal
 
 sed -i "/live_cnn/ c https://video-auth7.iol.pt/live_cnn/live_cnn/playlist.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)/" m3upt.m3u
