114dcf5bdbdfb680be047a778c81be28f9fd8f74
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2022-08-10 04:22:47 -0300

    Update streams.sh

--- streams.sh
+++ streams.sh
@@ -12,8 +12,4 @@ sed -i "/live_cnn/ c https://video-auth7.iol.pt/live_cnn/live_cnn/playlist.m3u8?
 
 sed -i "/live_tvi_internacional/ c https://video-auth6.iol.pt/live_tvi_internacional/live_tvi_internacional/playlist.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)/" m3upt.m3u
 
-# TVI Internacional - update the stream URL of TVI Reality
-
-sed -i "/live_tvi_direct/ c https://video-auth7.iol.pt/live_tvi_direct/live_tvi_direct/edge_servers/tvireality-720_passthrough/chunks.m3u8?nimblesessionid=184934045&wmsAuthSign$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)/" m3upt.m3u
-
 exit 0
