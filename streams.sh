0963ee3cf71cfbb8b8e9cd56326cfb643d3af4b0
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2023-01-14 07:24:06 -0300

    Update streams.sh

--- streams.sh
+++ streams.sh
@@ -1,25 +1,17 @@
 #!/bin/bash
 
-# Define the base URL
-base_url="https://video-auth6.iol.pt"
+# Define the function to update the stream URL
+update_url() {
+  channel=$1
+  url=$2
+  sed -i "s|$url|$url?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)|" m3upt.m3u
+  echo "Updated stream URL for $channel"
+}
 
-# Define the channels and their respective URLs
-channels=(
-  ["TVI"]="live_tvi/live_tvi/playlist.m3u8"
-  ["CNN Portugal"]="live_cnn/live_cnn/playlist.m3u8"
-  ["TVI Internacional"]="live_tvi_internacional/live_tvi_internacional/playlist.m3u8"
-  ["TVI Reality"]="live_tvi_direct/live_tvi_direct/edge_servers/tvireality-720_passthrough/playlist.m3u8"
-)
-
-# Obtain the authentication token
-token=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)
-
-# Update the URLs in the m3upt.m3u file
-for channel in "${channels[@]}"; do
-  name=${channel[0]}
-  url=${channel[1]}
-  sed -i "s|$base_url/$url|$base_url/$url?wmsAuthSign=$token|" m3upt.m3u
-  echo "Updated stream URL for $name"
-done
+# Update the stream URLs for each channel
+update_url "TVI" "https://video-auth6.iol.pt/live_tvi/live_tvi/playlist.m3u8"
+update_url "CNN Portugal" "https://video-auth7.iol.pt/live_cnn/live_cnn/playlist.m3u8"
+update_url "TVI Internacional" "https://video-auth6.iol.pt/live_tvi_internacional/live_tvi_internacional/playlist.m3u8"
+update_url "TVI Reality" "https://video-auth4.iol.pt/live_tvi_direct/live_tvi_direct/edge_servers/tvireality-720_passthrough/playlist.m3u8"
 
 exit 0
