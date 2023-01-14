47e8630e049ccb001b56b71ab6884dbfc9766f9c
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2023-01-14 07:26:00 -0300

    Update streams.sh

--- streams.sh
+++ streams.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
-# Define the function to update the stream URL
-update_url() {
-  channel=$1
-  url=$2
+# Define the configuration file
+config_file="./channels.cfg"
+
+# Check if the configuration file exists
+if [ ! -f $config_file ]; then
+  echo "Error: Configuration file $config_file does not exist."
+  exit 1
+fi
+
+# Read the configuration file
+while read line; do
+  # Skip comments and empty lines
+  [[ $line =~ ^#.*$ || -z $line ]] && continue
+  
+  # Split the line into an array
+  IFS='=' read -ra channel_info <<< "$line"
+  channel=${channel_info[0]}
+  url=${channel_info[1]}
+
+  # Update the stream URL
   sed -i "s|$url|$url?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)|" m3upt.m3u
   echo "Updated stream URL for $channel"
-}
-
-# Update the stream URLs for each channel
-update_url "TVI" "https://video-auth6.iol.pt/live_tvi/live_tvi/playlist.m3u8"
-update_url "CNN Portugal" "https://video-auth7.iol.pt/live_cnn/live_cnn/playlist.m3u8"
-update_url "TVI Internacional" "https://video-auth6.iol.pt/live_tvi_internacional/live_tvi_internacional/playlist.m3u8"
-update_url "TVI Reality" "https://video-auth4.iol.pt/live_tvi_direct/live_tvi_direct/edge_servers/tvireality-720_passthrough/playlist.m3u8"
+done < $config_file
 
 exit 0
